9b3f41f6e2c43d6c7b26ad6202b45526d3118159
Andrew Johnson <ajohnson.uva@gmail.com>
2019-04-29 18:15:38 -0400

    write seed code to import fair market rent data from .xls

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+if FreeMarketRentInfo.all.size == 0
+  xls = Roo::Excel.new("./FY2015F_4050_Final.xls")
+  sheet = xls.sheet(0)
+  sheet.each(state: 'state_alpha', county: 'countyname', fmr0: 'fmr0', fmr1: 'fmr1', fmr2: 'fmr2', fmr3: 'fmr3', fmr4: 'fmr4') do |hash|
+    FreeMarketRentInfo.create(hash)
+  end
+end
