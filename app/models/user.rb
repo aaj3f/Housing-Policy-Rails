0eb9704a15a80c58a9b7523b8564dfdc4dadc5a8
Andrew Johnson <ajohnson.uva@gmail.com>
2019-04-29 12:45:46 -0400

    create users table in db and add #calculate_median_income to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+
+  def calculate_median_income
+    base_url = 'https://api.census.gov'
+    queries = '/data/2017/acs/acs5?get=B19013_001E&for=zip%20code%20tabulation%20area:' + self.zipcode.to_s + '&key=' + ENV['CENSUS_API_KEY']
+    url = base_url + queries
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    json_response = JSON.parse(response)
+    median_income = json_response[1][0]
+    median_income
+  end
 end
--- db/migrate/20190429145114_create_users.rb
+++ db/migrate/20190429145114_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string, :ip_address
-      t.intger, :zipcode
-      t.integer, :salary
+      t.string :ip_address
+      t.integer :zipcode
+      t.integer :salary
       t.integer :rent_cost
 
       t.timestamps
