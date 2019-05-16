3d71bc812f0a8e55728ce98bffbeacda465f6134
Andrew Johnson <ajohnson.uva@gmail.com>
2019-05-16 13:12:39 -0400

    change cors uri access

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://housing-policy-react.herokuapp.com'
+    origins 'https://housing-policy.herokuapp.com'
 
     resource '*',
       headers: :any,
